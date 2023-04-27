14e54b7f1d76bc5259b42b11cb3cee1eb8c82d5f
lydiaimaa <rahmaranirani670@gmail.com>
2023-04-27 08:59:43 +0700

    (sahartian irma)

--- src/pages/team.jsx
+++ src/pages/team.jsx
@@ -7,16 +7,16 @@ const Team = () => {
         <section class="bg-white dark:bg-gray-900">
   <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
       <div class="mx-auto mb-8 max-w-screen-sm lg:mb-16">
-          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our team</h2>
-          <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">Explore the whole collection of open-source web components and elements built with the utility classes from Tailwind</p>
+          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Team</h2>
+          <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">Mengenal Team Kami lebih dekat</p>
       </div> 
       <div class="grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           <div class="text-center text-gray-500 dark:text-gray-400">
               <img class="mx-auto mb-4 w-36 h-36 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png" alt="Bonnie Avatar"></img>
               <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  <a href="#">Bonnie Green</a>
+                  <a href="#">Barik Octavia Aditama Putri </a>
               </h3>
-              <p>CEO/Co-founder</p>
+              <p>keterangan kerjaan</p>
               <ul class="flex justify-center mt-4 space-x-4">
                   <li>
                       <a href="#" class="text-[#39569c] hover:text-gray-900 dark:hover:text-white">
@@ -43,9 +43,9 @@ const Team = () => {
           <div class="text-center text-gray-500 dark:text-gray-400">
               <img class="mx-auto mb-4 w-36 h-36 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/helene-engels.png" alt="Helene Avatar"></img>
               <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  <a href="#">Helene Engels</a>
+                  <a href="#">Anisa Ramadani</a>
               </h3>
-              <p>CTO/Co-founder</p>
+              <p>Keterangan kerjaan</p>
               <ul class="flex justify-center mt-4 space-x-4">
                   <li>
                       <a href="#" class="text-[#39569c] hover:text-gray-900 dark:hover:text-white">
@@ -72,9 +72,9 @@ const Team = () => {
           <div class="text-center text-gray-500 dark:text-gray-400">
               <img class="mx-auto mb-4 w-36 h-36 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Avatar"></img>
               <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  <a href="#">Jese Leos</a>
+                  <a href="#">Sahartian Irma Rahmarani</a>
               </h3>
-              <p>SEO & Marketing</p>
+              <p>keterangan pekerjaan</p>
               <ul class="flex justify-center mt-4 space-x-4">
                   <li>
                       <a href="#" class="text-[#39569c] hover:text-gray-900 dark:hover:text-white">
@@ -101,9 +101,9 @@ const Team = () => {
           <div class="text-center text-gray-500 dark:text-gray-400">
               <img class="mx-auto mb-4 w-36 h-36 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/joseph-mcfall.png" alt="Joseph Avatar"></img>
               <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  <a href="#">Joseph Mcfall</a>
+                  <a href="#">Jessycha Devi Rudista</a>
               </h3>
-              <p>Sales</p>
+              <p>keterangan kerjaan</p>
               <ul class="flex justify-center mt-4 space-x-4">
                   <li>
                       <a href="#" class="text-[#39569c] hover:text-gray-900 dark:hover:text-white">
@@ -130,9 +130,9 @@ const Team = () => {
           <div class="text-center text-gray-500 dark:text-gray-400">
               <img class="mx-auto mb-4 w-36 h-36 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/sofia-mcguire.png" alt="Sofia Avatar"></img>
               <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  <a href="#">Lana Byrd</a>
+                  <a href="#">Merzy Anjani Ganadi</a>
               </h3>
-              <p>Web Designer</p>
+              <p>keteranagan pekerjaan</p>
               <ul class="flex justify-center mt-4 space-x-4">
                   <li>
                       <a href="#" class="text-[#39569c] hover:text-gray-900 dark:hover:text-white">
@@ -159,9 +159,9 @@ const Team = () => {
           <div class="text-center text-gray-500 dark:text-gray-400">
               <img class="mx-auto mb-4 w-36 h-36 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/thomas-lean.png" alt="Leslie Avatar"></img>
               <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  <a href="#">Leslie Livingston</a>
+                  <a href="#">Shifa Nurkholis</a>
               </h3>
-              <p>Graphic Designer</p>
+              <p>keteraangan pekerjaan</p>
               <ul class="flex justify-center mt-4 space-x-4">
                   <li>
                       <a href="#" class="text-[#39569c] hover:text-gray-900 dark:hover:text-white">
@@ -188,9 +188,9 @@ const Team = () => {
           <div class="text-center text-gray-500 dark:text-gray-400">
               <img class="mx-auto mb-4 w-36 h-36 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png" alt="Michael Avatar"></img>
               <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  <a href="#">Michael Gough</a>
+                  <a href="#">Aditya Hari Saputro</a>
               </h3>
-              <p>React Developer</p>
+              <p>keterangan pekerjaan</p>
               <ul class="flex justify-center mt-4 space-x-4">
                   <li>
                       <a href="#" class="text-[#39569c] hover:text-gray-900 dark:hover:text-white">
