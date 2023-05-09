12412aa609f4499484158a2a594d00afbeb5df04
lydiaimaa <rahmaranirani670@gmail.com>
2023-05-09 22:42:33 +0700

    SahartianIrma

--- src/pages/halamanadmin.jsx
+++ src/pages/halamanadmin.jsx
@@ -1,24 +1,34 @@
-import Anime from "../commponents/comAnime"
-import Drakor from "../commponents/comDrakor"
-import Footer from "../commponents/footer"
-import gambar1 from "../asset/anikor.png"
-import NavAdmin from "../commponents/navadmin"
+import React from "react";
+import logo from '../asset/logorpl2.png';
+import {AiOutlineTime,}from "react-icons/ai";
+import {HiOutlineUserGroup, HiUserGroup}from "react-icons/hi";
 
-const HomeAdmin = () => {
-    return(
-        <>
-        <div>
-            <NavAdmin />
-            <div className='bg-inherit'>
-                <img src={gambar1} className='w-full xl:h-[667px] md:h-full'></img>
-            </div>
-            <Anime />
-            <Drakor />
-            <Footer />
+export default function Sidebar(){
+    const menu = [
+        { name: "Team", icon: <AiOutlineTime/> },
+        { name: "Anime", icon: <HiOutlineUserGroup/> },
+        { name: "Drakor", icon: <HiUserGroup/> },
+    ];
+
+    return (
+    <div className="h-screen border-r border-gray-200 w-64 px-9">
+        <div className="flex flex-row items-center">
+            <img src={logo} alt="logorpl" className="w-9 h-9"></img>
+            <div>Anikor</div>
         </div>
-        </>
-        
-    )
+        <div>
+            <ul>
+            <div className="mb-4">Menu</div>
+                {menu.map((val, index) => {
+                 return (
+                     <li key={index} className="mb-7 flex flex-row items-center">
+                      <div className="mr-5">{val.icon}</div>
+                      <div>{val.name}</div>
+                 </li>
+                );
+                })}
+            </ul>
+        </div>
+    </div>
+    );
 }
-
-export default HomeAdmin
