c8d3943decaa4846a77fa2b7dec9c017408dfc21
anisaramadani <ramadanianisa977@gmail.com>
2023-06-06 07:35:15 +0700

    (anisa)

--- src/commponents/datawhoare.js
+++ src/commponents/datawhoare.js
@@ -3,37 +3,37 @@ export const dataschool = [
       id: "1",
       name: 'Kim So-hyun',
       peran: 'Lee Eun Bi',
-      image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fasianwiki.com%2FKim_So-Hyun_%25281999%2529&psig=AOvVaw2iJZXt7z_9qJy9JPwbPL4W&ust=1683704120320000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCMizguPc5_4CFQAAAAAdAAAAABAE',
+      image: 'https://i0.wp.com/www.jauhari.net/engine/wp-content/uploads/2016/09/Koleksi-Lengkap-Album-Foto-Terbaru-Kim-So-Hyun-Artis-Cantik-Korea-18.jpg?fit=1080%2C966&ssl=1',
     },
     {
       id: "2",
       name: 'Nam Joo-hyuk',
       peran: 'Han Yi An',
-      image: 'https://th.bing.com/th/id/R.1752c935ccd3e23350aa3401b6ed9cd3?rik=oLvhWJRVi8SR0g&riu=http%3a%2f%2fi.imgur.com%2fn7QSYzk.jpg&ehk=DHtGuVwn5cFmrvsYgqlLXgY5iIGSKebRhFM2PR2pUoQ%3d&risl=&pid=ImgRaw&r=0',
+      image: 'https://tse3.mm.bing.net/th?id=OIP.pnHxuCJeENN2ZZRMdVlvvgHaI6&pid=Api&P=0&h=180',
     },
     {
       id: "3",
       name: 'Yook Sung-jae',
       peran: 'Gong Tae-kwang',
-      image: 'https://yt3.ggpht.com/a/AATXAJwcDSx5Vw0C7CKQQVXavXVhAZVeqGYL0ile1g=s900-c-k-c0xffffffff-no-rj-mo',
+      image: 'https://tse3.mm.bing.net/th?id=OIP.B3CDAmnqnzNlxbmQw9v84AHaE8&pid=Api&P=0&h=180',
     },
     {
       id: "4",
       name: 'Kim So-hyun',
       peran: 'Go Eun-byul',
-      image: 'https://lh3.googleusercontent.com/proxy/Dbbgh3YQXd109S6mE6wjaAEiuVjHA1hStMXGl17ixIQcz0w60RMEdeBSGaQyKf5yZ04uzaI_RvcIl2YwzAv_GnvuZQhmFLAWomBtMKymapg-avPHIsLYRQUosMMTni1xDdSw4lK1mDynW3i1CxYikqJHd-r-ddb3jDLB-TNKIJ3Jb96yjQQ2QW1ITgQFK_JihnVTggp2=w1200-h630-p-k-no-nu',
+      image: 'https://tse3.mm.bing.net/th?id=OIP.l3FOPO1o-6UEphYt27q-3gHaE0&pid=Api&P=0&h=180',
     },
     {
       id: "5",
       name: 'Jeon Mi Seon',
       peran: 'Song Mi Gyeong',
-      image: 'https://i.ytimg.com/vi/c8eiVrHNA4o/maxresdefault.jpg',
+      image: 'https://tse1.mm.bing.net/th?id=OIP.8izuJ8F6xBgIj6w2_WDsQQHaE8&pid=Api&P=0&h=180',
     },
     {
       id: "6",
       name: 'Jo Byeong-gyu',
       peran: 'Byeong-gyu',
-      image: 'https://img1.ak.crunchyroll.com/i/spire4/e29bbc34b895c20160775e2ad1373b0c1583366454_full.jpg',
+      image: 'https://tse4.mm.bing.net/th?id=OIP.eq9fa9wjsjPYeuDrt_pcXQHaFU&pid=Api&P=0&h=180',
     },
     
   ];    
